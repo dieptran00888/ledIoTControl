16d0a77d1df6b27e07af8bec243a9a94a258198f
dieptran00888 <dieptx.ptit@gmail.com>
2017-08-28 16:50:17 +0700

    Initial Commit

---
+++ LedInternetOfThing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LedInternetOfThing
+//
+//  Created by tran.xuan.diep on 8/28/17.
+//  Copyright © 2017 tran.xuan.diep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
