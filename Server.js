55e198266a7639d8447bd44e308a4b41fc4c35e8
dieptran00888 <dieptx.ptit@gmail.com>
2017-08-31 08:48:57 +0700

    Add server nodejs

---
+++ Server.js
@@ -0,0 +1,15 @@
+var http = require("http");
+var url = require("url");
+var path = require("path");
+var ServerIP = '127.0.0.1',
+    port = '8080';
+
+var Server = http.createServer(function (request , response) {
+    console.log("Request Recieved" + request.url);
+    console.log(request.url.substring(6, 8))
+    var SampleJsonData = JSON.stringify([{"ElementName":"ElementValue"}]);
+    response.end('_testcb(' + SampleJsonData + ')'); // this is the postbackmethod
+   }); 
+Server.listen(port, ServerIP, function () {
+    console.log("Listening..." + ServerIP + ":" + port);
+});
